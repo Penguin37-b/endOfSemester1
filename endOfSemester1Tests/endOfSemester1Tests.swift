68144647a154a23a4dcf15ad71c4dc20ddd59b25
Arsen <147736@gmail.com>
2024-11-13 09:36:14 -0500

    Initial Commit

---
+++ endOfSemester1Tests/endOfSemester1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  endOfSemester1Tests.swift
+//  endOfSemester1Tests
+//
+//  Created by Arsen Suchek on 11/13/24.
+//
+
+import Testing
+@testable import endOfSemester1
+
+struct endOfSemester1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
